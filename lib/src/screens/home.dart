2f3d73178da4a0cb5f6a3f475ba5ba2e9a174961
Vijay <vijay@starnik.com>
2021-08-02 23:47:16 +0530

    committing as on 08/02/2021.

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import '../widgets/cat.dart';
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> with TickerProviderStateMixin {
+  late Animation<double> catAnimation;
+  late AnimationController catController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    catController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(CurvedAnimation(
+      parent: catController,
+      curve: Curves.easeIn,
+    ));
+  }
+
+  onTap() {
+    if (catController.status == AnimationStatus.completed) {
+      catController.reverse();
+    } else if (catController.status == AnimationStatus.dismissed) {
+      catController.forward();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Animiation!'),
+      ),
+      body: GestureDetector(
+        child: Center(
+          child: Stack(
+            children: [
+              buildCatAnimation(),
+              buildBox(),
+            ],
+          ),
+        ),
+        onTap: onTap,
+      ),
+    );
+  }
+
+  Widget buildCatAnimation() {
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (context, child) {
+        return Container(
+          child: child,
+          margin: EdgeInsets.only(top: catAnimation.value),
+        );
+      },
+      child: Cat(),
+    );
+  }
+
+  Widget buildBox() {
+    return Container(
+      height: 200.0,
+      width: 200.0,
+      color: Colors.brown,
+    );
+  }
+}
